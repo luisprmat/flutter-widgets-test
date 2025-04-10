d0e303de9775dad53981e6a0219ded5046a95ac8
Luis Parrado <luisprmat@gmail.com>
2025-04-10 18:16:21 -0500

    Controlled Progress indicators (Simulate with Stream builder)

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -31,9 +31,9 @@ class _ProgressView extends StatelessWidget {
           ),
 
           SizedBox(height: 20),
-          Text('Circular progress indicator controlado'),
+          Text('Circular y Linear controlado'),
           SizedBox(height: 10),
-          _ControlledProgressIndicator()
+          _ControlledProgressIndicator(),
         ],
       ),
     );
@@ -45,6 +45,30 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder<double>(
+      stream: Stream.periodic(
+        const Duration(milliseconds: 300),
+        (value) => (value * 2) / 100,
+      ).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12,
+              ),
+              const SizedBox(width: 20),
+              Expanded(child: LinearProgressIndicator(value: progressValue)),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
