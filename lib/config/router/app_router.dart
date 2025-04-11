49a7031208d8dfc4fefb3c36b960bb87a5efb1a1
Luis Parrado <luisprmat@gmail.com>
2025-04-10 22:22:44 -0500

    Tutorial assets + add router + menu item

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -39,5 +39,10 @@ final appRouter = GoRouter(
       name: UiControlsScreen.name,
       builder: (context, state) => UiControlsScreen(),
     ),
+    GoRoute(
+      path: '/tutorial',
+      name: AppTutorialScreen.name,
+      builder: (context, state) => AppTutorialScreen(),
+    ),
   ],
 );
