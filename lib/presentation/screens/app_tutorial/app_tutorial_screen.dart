dc7b56985f8ad07801dffc1fc86c2b4a3d61e6eb
Luis Parrado <luisprmat@gmail.com>
2025-04-10 23:22:45 -0500

    Add styles and alignments to ListView Tutorial

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,12 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    'Busca la comida',
+    'Ex est nostrud pariatur ad.',
+    'assets/images/1.png',
+  ),
+  SlideInfo(
+    'Entrega rápida',
+    'Ut ea consequat nisi officia sint consequat officia officia tempor.',
+    'assets/images/2.png',
+  ),
+  SlideInfo(
+    'Disfruta la comida',
+    'Cillum irure irure excepteur proident incididunt cupidatat.',
+    'assets/images/3.png',
+  ),
+];
 
 class AppTutorialScreen extends StatelessWidget {
   static const String name = 'tutorial_screen';
-  
+
   const AppTutorialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            children:
+                slides
+                    .map(
+                      (slideData) => _Slide(
+                        title: slideData.title,
+                        caption: slideData.caption,
+                        imageUrl: slideData.imageUrl,
+                      ),
+                    )
+                    .toList(),
+          ),
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              onPressed: () => context.pop(),
+              child: const Text('Salir'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            const SizedBox(height: 20),
+            Text(title, style: titleStyle),
+            const SizedBox(height: 10),
+            Text(caption, style: captionStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
