ea5d4fdcdbacb24a277df0ca9a50ff69b81ad8da
Luis Parrado <luisprmat@gmail.com>
2025-04-15 00:38:36 -0500

    Screen to change Theme Color

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
+
+// Inmutable color list
+final colorListProvider = Provider<List<Color>>((ref) => colorList);
