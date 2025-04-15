0adf4fca0baa3a1ef562f96d8a9419ff5bbd62a8
Luis Parrado <luisprmat@gmail.com>
2025-04-14 23:28:17 -0500

    Install and setup riverpod for Flutter and create provider (counter)

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(title: Text('Counter Screen')),
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge),
+        child: Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
